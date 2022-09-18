8d18811cab4ed513689b029c031d07a22d12a575
hoang ryan <judaime36@gmail.com>
2022-09-19 00:32:15 +0700

    second

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 console.log("hello world");
 console.log("hello world 2");
+
+//? git command to commit
+//* git remote add origin <url-reponsity>
+//* git add .
+//* git commit -m "what ever you want"
+//? push code 
+//* git push -u origin master
+//? and update code
+//* git pull
+ 
